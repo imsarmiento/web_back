a743385f227ae85fc3ea5d92efcd2eece972df9e
Oscar Julian Castañeda Gutierrez <oj.castaneda@uniandes.edu.co>
2021-04-10 22:41:02 -0500

    Get populate y patch evento y regla existentes

--- back/src/routes/evento.js
+++ back/src/routes/evento.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Evento = require("../models/evento-model");
+const Regla = require("../models/regla-model")
 const router = new express.Router();
 
 router.get("/:id", async (req, res) => {
@@ -59,5 +60,39 @@ router.delete("/:id", async (req, res) => {
         return res.status(500).send(error);
     }
 });
+
+router.get("/:id/reglas", async (req, res) => {
+    try {
+        const evento = await Evento.findById(req.params.id).populate("reglas");
+        if (!evento) {
+            return res.status(404).send("No hubo coincidencia de evento");
+        }
+        return res.status(200).send(evento);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+router.patch("/:id/reglas/:idRegla", async (req, res) => {
+    try {
+        const evento = await Evento.findById(req.params.id);
+        if (!evento) {
+            return res.status(404).send("No hubo coincidencia de evento");
+        }
+        console.log(evento);
+        const regla = await Regla.findById(req.params.idRegla);
+        if (!regla) {
+            return res.status(404).send("No hubo coincidencia de regla");
+        }
+        console.log(regla);
+        evento.reglas.push(regla);
+        evento.save();
+        return res.send(evento);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send(error);
+    }
+});
+
 
 module.exports = router;
