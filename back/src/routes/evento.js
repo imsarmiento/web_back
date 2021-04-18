07a3f1e3056a76caee575e5805cd419493296ec3
Oscar Julian Castañeda Gutierrez <oj.castaneda@uniandes.edu.co>
2021-04-18 11:03:08 -0500

    Verificacion de disponibilidad

--- back/src/routes/evento.js
+++ back/src/routes/evento.js
@@ -272,5 +272,141 @@ router.post("/crearEventoCompleto", async (req, res) => {
     }
 );
 
+/**
+ * Verifica un evento para un grupo de usuarios con correos especificados si se puede agendar a todos
+ */
+router.post("/verificarDisponibilidad", async (req, res) => {
+        try {
+            let eventos = [];
+            let usuarios = [];
+            let usuariosParam = req.body.correos;
+            let eventoParam = req.body.evento;
+            let size = usuariosParam.length;
+            for (let i = 0; i < size; i++) {
+                const usuario = await Usuario.findOne({correo: usuariosParam[i]}).populate({
+                    path: "eventos",
+                    $match: {
+                        $or: [
+                            {diaInicio: {$gte: eventoParam.start}},
+                            {diaFin: {$lte: eventoParam.end}}]
+                    },
+                    populate: {
+                        path: "reglas",
+                    }
+                });
+                if (!usuario) {
+                    return res
+                        .status(404)
+                        .send({error: "No existe un usuario con el correo " + usuariosParam[i]});
+                }
+                usuarios.push(usuario);
+                eventos.concat(usuario.eventos);
+                usuario.eventos.forEach((evento) => {
+                    const diaFin = new Date(eventoParam.diaFin);
+                    if (evento.frecuencia === "sinRepetir") {
+                        let sizeEvento = evento.reglas.length;
+                        for (let j = 0; j < sizeEvento; j++) {
+                            const regla = evento.reglas[j];
+                            let diaIterador = new Date(regla.horaInicio);
+                            let diaIteradorFin = new Date(regla.horaFin);
+                            eventos.push({start: diaIterador, end: diaIteradorFin});
+                        }
+                    } else if (evento.frecuencia === "semanal") {
+                        let sizeEvento = evento.reglas.length;
+                        for (let j = 0; j < sizeEvento; j++) {
+                            const regla = evento.reglas[j];
+                            let diaIterador = new Date(regla.horaInicio);
+                            let diaIteradorFin = new Date(regla.horaFin);
+                            diaIterador.setDate(diaIterador.getDate() + regla.unidad);
+                            diaIteradorFin.setDate(diaIteradorFin.getDate() + regla.unidad);
+                            while ((diaFin > diaIterador) && (evento.diaFin > diaIterador)) {
+                                eventos.push({start: new Date(diaIterador), end: new Date(diaIteradorFin)});
+                                diaIterador.setDate(diaIterador.getDate() + 7);
+                                diaIteradorFin.setDate(diaIteradorFin.getDate() + 7);
+                            }
+
+                        }
+                    } else if (evento.frecuencia === "mensual") {
+                        let sizeEvento = evento.reglas.length;
+                        for (let j = 0; j < sizeEvento; j++) {
+                            const regla = evento.reglas[j];
+                            let diaIterador = new Date(regla.horaInicio);
+                            let diaIteradorFin = new Date(regla.horaFin);
+                            diaIterador.setDate(diaIterador.getDate() + regla.unidad);
+                            diaIteradorFin.setDate(diaIteradorFin.getDate() + regla.unidad);
+                            while ((diaFin > diaIterador) && (evento.diaFin > diaIterador)) {
+                                eventos.push({start: new Date(diaIterador), end: new Date(diaIteradorFin)});
+                                diaIterador.setMonth(diaIterador.getMonth() + 1);
+                                diaIteradorFin.setMonth(diaIteradorFin.getMonth() + 1);
+                            }
+                        }
+                    }
+
+                });
+            }
+            size = eventoParam.reglas.length;
+            const diaFin = new Date(eventoParam.diaFin);
+            let eventosSize = eventos.length;
+            for (let i = 0; i < size; i++) {
+                const regla = eventoParam.reglas[i];
+                for (let j = 0; j < eventosSize; j++) {
+                    if (eventoParam.frecuencia === "sinRepetir") {
+                        const reglaInicio = new Date(regla.horaInicio);
+                        const reglaFin = new Date(regla.horaFin);
+                        if (reglaFin <= eventos[j].end && reglaFin >= eventos[j].start) {
+                            return res
+                                .status(500)
+                                .send({error: "Existe un conflicto de horario"});
+                        } else if (reglaInicio <= eventos[j].end && reglaInicio >= eventos[j].start) {
+                            return res
+                                .status(500)
+                                .send({error: "Existe un conflicto de horario"});
+                        }
+                    } else if (eventoParam.frecuencia === "semanal") {
+                        const reglaInicio = new Date(regla.horaInicio);
+                        const reglaFin = new Date(regla.horaFin);
+                        reglaInicio.setDate(reglaInicio.getDate() + regla.unidad);
+                        reglaFin.setDate(reglaFin.getDate() + regla.unidad);
+                        while (reglaInicio <= diaFin) {
+                            if (reglaFin <= eventos[j].end && reglaFin >= eventos[j].start) {
+                                return res
+                                    .status(500)
+                                    .send({error: "Existe un conflicto de horario"});
+                            } else if (reglaInicio <= eventos[j].end && reglaInicio >= eventos[j].start) {
+                                return res
+                                    .status(500)
+                                    .send({error: "Existe un conflicto de horario"});
+                            }
+                            reglaInicio.setDate(reglaInicio.getDate() + 7);
+                            reglaFin.setDate(reglaFin.getDate() + 7);
+                        }
+                    } else if (eventoParam.frecuencia === "mensual") {
+                        const reglaInicio = new Date(regla.horaInicio);
+                        const reglaFin = new Date(regla.horaFin);
+                        reglaInicio.setDate(reglaInicio.getDate() + regla.unidad);
+                        reglaFin.setDate(reglaFin.getDate() + regla.unidad);
+                        while (reglaInicio <= diaFin) {
+                            if (reglaFin <= eventos[j].end && reglaFin >= eventos[j].start) {
+                                return res
+                                    .status(500)
+                                    .send({error: "Existe un conflicto de horario"});
+                            } else if (reglaInicio <= eventos[j].end && reglaInicio >= eventos[j].start) {
+                                return res
+                                    .status(500)
+                                    .send({error: "Existe un conflicto de horario"});
+                            }
+                            reglaInicio.setMonth(reglaInicio.getMonth() + 1);
+                            reglaFin.setMonth(reglaFin.getMonth() + 1);
+                        }
+                    }
+                }
+                res.status(200).send({mensaje:"No existen conflictos de horario para este evento"})
+            }
+        } catch
+            (error) {
+            return res.status(400).send({error: error});
+        }
+    }
+);
 
 module.exports = router;
